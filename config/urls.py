f1327519acb335a4bc4b8097af3ae29c7a0e7131
1F10250263 <s1f102502632@iniad.org>
2026-07-02 17:16:38 +0900

    削除機能追加

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
     path('<int:task_id>/close', todo_views.close, name='close'),
-    path('<int:task_id>/update', todo_views.update, name='update')
+    path('<int:task_id>/update', todo_views.update, name='update'),
+    path('task/<int:task_id>/delete/',todo_views.delete_task, name='delete'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
-
 # Create your views here.
 
 
@@ -60,3 +59,7 @@ def update(request, task_id):
     }
     return render(request, 'todo/edit.html', context)
 
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('index')
